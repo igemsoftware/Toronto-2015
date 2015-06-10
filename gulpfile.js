b15dd64abd00300004d1c6534f4b7c0b598d1a19
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-06-10 15:45:45 -0400

    compass-mixins works now

--- gulpfile.js
+++ gulpfile.js
@@ -12,40 +12,22 @@ var indexDest = './src';
 var sassDir = './src/styles/sass/*.scss';
 var cssDir = './src/styles';
 
-/**
- * ==== wiredep ====
- *
- * Takes css and js from bower_components and inserts into index.html
- */
+// runs on bower postinstall hook
 gulp.task('wiredep', function() {
     gulp.src(index).pipe(wiredep()).pipe(gulp.dest(indexDest));
 });
 
-
-/**
- * ==== inject-css ====
- *
- * Inserts link tag for any css files in ./src/styles into index.html
- */
 gulp.task('inject-css', function() {
     var sources = gulp.src('./src/styles/*.css', {read: false});
     gulp.src(index).pipe(inject(sources, {relative: true})).pipe(gulp.dest(indexDest));
 });
 
-
-/**
- * ==== sass ====
- *
- * Compile styles.scss into style.css with node-sass (libsass).
- */
 gulp.task('sass', function() {
     gulp.src(sassDir)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(cssDir))
     .pipe(browserSync.stream());
 });
-
-
 
 gulp.task('serve', function() {
     browserSync.init({
@@ -61,12 +43,6 @@ gulp.task('serve', function() {
     gulp.watch('./src/**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['sass', 'serve']);
 
-/**
-* ==== inject ====
-*
-* The main inject task.
-* Doesn't work from blank file?
-*/
 gulp.task('inject', ['wiredep', 'inject-css']);
