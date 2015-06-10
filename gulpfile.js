0f2dc5f0df4bcd82236aac071b72bba668555b07
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-06-10 14:40:46 -0400

    sass and watch:sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var inject = require('gulp-inject');
+var sass = require('gulp-sass');
 
 // ==== src and dest for index.html ====
 var index = './src/index.html';
 var indexDest = './src';
-
+// ==== scss and css dirs ====
+var sassDir = './src/styles/sass/*.scss';
+var cssDir = './src/styles';
 
 /**
  * ==== wiredep ====
@@ -27,6 +30,26 @@ gulp.task('inject-css', function() {
     var sources = gulp.src('./src/styles/*.css', {read: false});
     gulp.src(index).pipe(inject(sources, {relative: true})).pipe(gulp.dest(indexDest));
 });
+
+
+/**
+ * ==== sass ====
+ *
+ * Compile styles.scss into style.css with node-sass (libsass).
+ */
+gulp.task('sass', function() {
+    gulp.src(sassDir).pipe(sass().on('error', sass.logError)).pipe(gulp.dest(cssDir));
+});
+
+/**
+* ==== watch:sass ====
+*
+* Calls sass task on filechange in sassDir.
+*/
+gulp.task('watch:sass', function() {
+   gulp.watch(sassDir, ['sass']);    
+});
+
 
 /**
 * ==== inject ====
