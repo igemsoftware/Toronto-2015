78bbe832ed7f2d9a1b5e69f535c214ff3d8b1b6c
ACalza <albert.calzaretto@gmail.com>
2015-06-23 18:35:10 -0400

    Added retrieve for a specific species, returns a generic JSON object, due to reserved words, the column names are capitalized

--- routes/species/retrieve/index.js
+++ routes/species/retrieve/index.js
@@ -5,7 +5,6 @@ var retrieve = App.Lib('speciesRetrieve');
 
 var retrieveElement = function(req, res, next) {
 	retrieve(req.query, function(err, data){
-		console.log("here");
 		if(err)
 			return res.send(err);
 		res.send(data)
