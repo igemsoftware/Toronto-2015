ef0c8fb94be47876e279e84273e19963d9312300
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-06-25 15:59:34 -0400

    basic Network class

--- src/app/components/network/network.controller.js
+++ src/app/components/network/network.controller.js
@@ -5,48 +5,53 @@ angular.module('fbaApp')
 .controller('NetworkCtrl', ['$scope', '$http', function($scope, $http) {
 
     var dataRequest = $http.get('http://45.55.193.224/toydata.json');
-
-
+    
+    // ==== Network Class ====
+    Network.getInstance('#network');
+    Network.test(); 
+        
     dataRequest.success(function(data) {
-        var data = data.data; 
+        /*
+         * var data = data.data; 
 
-        doShit();
+         * doShit();
 
-        var nodes = [
-            {id: "a", name: "A", selflink: false},
-            {id: "b", name: "B", selflink: true},
-            {id: "c", name: "C", selflink: false},
-            {id: "d", name: "D", selflink: false},
-            {id: "e", name: "E", selflink: false}
-        ];
+         * var nodes = [
+         *     {id: "a", name: "A", selflink: false},
+         *     {id: "b", name: "B", selflink: true},
+         *     {id: "c", name: "C", selflink: false},
+         *     {id: "d", name: "D", selflink: false},
+         *     {id: "e", name: "E", selflink: false}
+         * ];
 
-        console.log(data);
+         * console.log(data);
 
-        var view = '#network';
+         * var view = '#network';
 
-        var w = $(view).width();
-        var h = $(view).height();
+         * var w = $(view).width();
+         * var h = $(view).height();
 
-        var svg = d3.select(view).append('svg').attr('width', w).attr('height', h);
+         * var svg = d3.select(view).append('svg').attr('width', w).attr('height', h);
 
-        var force = d3.layout.force()
-            .size([w, h])
-            .nodes(nodes)
-            .charge(-100)
-            .start();
+         * var force = d3.layout.force()
+         *     .size([w, h])
+         *     .nodes(nodes)
+         *     .charge(-100)
+         *     .start();
 
-        var node = svg.selectAll('.node')
-            .data(nodes)
-            .enter().append('circle')
-            .attr('class', 'node')
-            .attr('r', 20)
-            .style('fill', 'red')
-            .call(force.drag);
+         * var node = svg.selectAll('.node')
+         *     .data(nodes)
+         *     .enter().append('circle')
+         *     .attr('class', 'node')
+         *     .attr('r', 20)
+         *     .style('fill', 'red')
+         *     .call(force.drag);
 
-        force.on('tick', function() {
-            node.attr('cx', function(d) { return d.x; })
-                .attr('cy', function(d) { return d.y; })
-        })
+         * force.on('tick', function() {
+         *     node.attr('cx', function(d) { return d.x; })
+         *         .attr('cy', function(d) { return d.y; })
+         * })
+         */
 
     }).error(function(err) {
         alert(err);
--- src/assets/js/d3network.js
+++ src/assets/js/d3network.js
@@ -1,3 +1,27 @@
-function doShit() {
-    alert('sweg');
-}
+var Network = (function() {
+    var instance = null;
+    
+    function Network() {
+        this.view = '';
+    }
+
+
+    function getInstance(divName) {
+        if (!instance) {
+            instance = new Network();
+            instance.view = divName;
+        }
+
+        return instance;
+    }
+
+
+    return {
+        getInstance: function(divName) {
+            getInstance(divName);
+        },
+        test: function() {
+            alert(instance.view);
+        }
+    };
+})();
