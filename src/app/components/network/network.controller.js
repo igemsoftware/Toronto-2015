c9d1fde7f494975836d5bd02260ff4e1143d8b4b
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-06-25 16:22:40 -0400

    classifyed with working d3 nodes

--- src/app/components/network/network.controller.js
+++ src/app/components/network/network.controller.js
@@ -2,57 +2,15 @@
 
 angular.module('fbaApp')
 
-.controller('NetworkCtrl', ['$scope', '$http', function($scope, $http) {
-
-    var dataRequest = $http.get('http://45.55.193.224/toydata.json');
+.controller('NetworkCtrl', ['$scope', '$http', function($scope, $http) { 
     
     // ==== Network Class ====
-    Network.getInstance('#network');
-    Network.test(); 
+    Network.getInstance('#network'); 
         
+    // for later 
+    var dataRequest = $http.get('http://45.55.193.224/toydata.json');
     dataRequest.success(function(data) {
-        /*
-         * var data = data.data; 
-
-         * doShit();
-
-         * var nodes = [
-         *     {id: "a", name: "A", selflink: false},
-         *     {id: "b", name: "B", selflink: true},
-         *     {id: "c", name: "C", selflink: false},
-         *     {id: "d", name: "D", selflink: false},
-         *     {id: "e", name: "E", selflink: false}
-         * ];
-
-         * console.log(data);
-
-         * var view = '#network';
-
-         * var w = $(view).width();
-         * var h = $(view).height();
-
-         * var svg = d3.select(view).append('svg').attr('width', w).attr('height', h);
-
-         * var force = d3.layout.force()
-         *     .size([w, h])
-         *     .nodes(nodes)
-         *     .charge(-100)
-         *     .start();
-
-         * var node = svg.selectAll('.node')
-         *     .data(nodes)
-         *     .enter().append('circle')
-         *     .attr('class', 'node')
-         *     .attr('r', 20)
-         *     .style('fill', 'red')
-         *     .call(force.drag);
-
-         * force.on('tick', function() {
-         *     node.attr('cx', function(d) { return d.x; })
-         *         .attr('cy', function(d) { return d.y; })
-         * })
-         */
-
+        console.log(data);
     }).error(function(err) {
         alert(err);
     }); 
--- src/assets/js/d3network.js
+++ src/assets/js/d3network.js
@@ -3,6 +3,18 @@ var Network = (function() {
     
     function Network() {
         this.view = '';
+        this.w = null;
+        this.h = null;
+        this.svg = null;
+        this.node = null;
+        this.nodes = [
+             {id: "a", name: "A", selflink: false},
+             {id: "b", name: "B", selflink: true},
+             {id: "c", name: "C", selflink: false},
+             {id: "d", name: "D", selflink: false},
+             {id: "e", name: "E", selflink: false}
+        ];
+        this.force = null;
     }
 
 
@@ -10,18 +22,46 @@ var Network = (function() {
         if (!instance) {
             instance = new Network();
             instance.view = divName;
+            instance.w = $(instance.view).width();
+            instance.h = $(instance.view).height(); 
         }
 
         return instance;
+    }
+
+    function createSvg() {
+        instance.svg = d3
+            .select(instance.view)
+            .append('svg')
+            .attr('width', instance.w)
+            .attr('height', instance.h);
+
+        instance.force = d3.layout.force()
+             .size([instance.w, instance.h])
+             .nodes(instance.nodes)
+             .charge(-100)
+             .start();
+
+        instance.node = instance.svg.selectAll('.node')
+             .data(instance.nodes)
+             .enter().append('circle')
+             .attr('class', 'node')
+             .attr('r', 20)
+             .style('fill', 'red')
+             .call(instance.force.drag);
+
+        instance.force.on('tick', function() {
+             instance.node
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+        })
     }
 
 
     return {
         getInstance: function(divName) {
             getInstance(divName);
-        },
-        test: function() {
-            alert(instance.view);
+            createSvg();
         }
     };
 })();
