8ce39a3fe9d8b5a7de5294dbb36ececbd3f32ae2
Mark Wang <peiqi1122@gmail.com>
2015-07-07 00:10:39 -0400

    first complete model in data tab: click draw to view sean and ghazah's d3network

---
+++ src/app/components/forcelayout/layout.controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('fbaApp')
+
+.controller('LayoutCtrl', ['$scope', '$http', 'UrlProvider',
+        function($scope, $http, UrlProvider) {
+    $scope.title = 'Layout component';
+    $scope.data = null;
+
+    $scope.draw = function(){
+        $http.get(UrlProvider.baseUrl + '/fba/getJSON').success(function(jsondata){
+            $scope.data = jsondata;
+            console.log('got json data');
+            forcelayout($scope.data);
+        });
+
+
+    };
+
+
+
+}]);
--- src/app/pages/data/data.controller.js
+++ src/app/pages/data/data.controller.js
@@ -4,14 +4,4 @@ angular.module('fbaApp')
 
 .controller('DataCtrl', ['$scope', '$http', 'UrlProvider', function($scope, $http, UrlProvider) {
     $scope.title = 'Data page';
-    $scope.data = '';
-
-    $http.get(UrlProvider.baseUrl + '/fba/getSBML').success(function(jsondata){
-        console.log(jsondata)
-        window.fba = jsondata;
-        $scope.data = jsondata
-    });
-
-
-
 }]);
