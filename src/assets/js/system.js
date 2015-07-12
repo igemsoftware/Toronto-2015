3dffd3a0935a8cbcebb8fd89886ce5b82b28a1b6
ACalza <albert.calzaretto@gmail.com>
2015-07-12 12:49:47 -0400

    Patched up those little 'errors' from gulp

--- src/assets/js/system.js
+++ src/assets/js/system.js
@@ -1,5 +1,6 @@
 var System = function(attributes, system){
-  var private = {
+  "use strict";
+  var _private = {
     network: null,
     nodes: null,     //all node elements class:node under <g> class:nodes
     links: null,    //all link elements class:link under <g> class:nodes
@@ -9,49 +10,49 @@ var System = function(attributes, system){
     linkSet: [],    //link data
     nodesSet: [],   //node data
     metaboliteRadius: 10
-  }
+  };
   //initalize Pathway
   init(system);
   //init
   function init(system){
-      //assign selection to private variables
-      private.network = d3.select('svg').select('.network') ;
+      //assign selection to _private variables
+      _private.network = d3.select('svg').select('.network') ;
       //create system tag
-      private.system = d3.select("svg").select(".network").append("g").attr("class", "system");
+      _private.system = d3.select("svg").select(".network").append("g").attr("class", "system");
       //create HTML nodes tags and
-      private.links = private.system.append("g").attr("class", "links");
-      private.nodes = private.system.append("g").attr("class", "nodes");
+      _private.links = _private.system.append("g").attr("class", "links");
+      _private.nodes = _private.system.append("g").attr("class", "nodes");
       //Create metabolite objects
       buildMetabolites(system);
       //Create reaction objects
       buildReactions(system);
       // initiate force
-      private.force = d3.layout.force()
-                          .nodes(private.nodesSet)
-                          .links(private.linkSet)
+      _private.force = d3.layout.force()
+                          .nodes(_private.nodesSet)
+                          .links(_private.linkSet)
                           .charge(-500)
                           .linkStrength(2)
                           .linkDistance(50)
-                          .size([private.attributes['svg']['width'], private.attributes['svg']['height']])
+                          .size([_private.attributes.svg.width, _private.attributes.svg.height])
                           .start()
                           .on("tick", tick);
       //adds data for links
-      private.links = private.links.selectAll("line").data(private.linkSet);
+      _private.links = _private.links.selectAll("line").data(_private.linkSet);
       //and binds data for nodes
-      private.nodes = private.nodes.selectAll(".node").data(private.nodesSet);
+      _private.nodes = _private.nodes.selectAll(".node").data(_private.nodesSet);
       //dragging
-      var drag = private.force.drag().on("dragstart", function(d){
+      var drag = _private.force.drag().on("dragstart", function(d){
         d3.event.sourceEvent.stopPropagation();
         d3.select(this).classed("fixed", d.fixed = true);
       });
-      private.nodes.call(drag);
+      _private.nodes.call(drag);
       draw();
   }
   function buildMetabolites(system){
       // loop and bind metabolite data to metabolite node to nodeset
       for (var i = 0; i<system.metabolites.length; i++){
-        private.nodesSet.push(new Metabolite(system.metabolites[i].name,
-                                                  system.metabolites[i].id, private.metaboliteRadius));
+        _private.nodesSet.push(new Metabolite(system.metabolites[i].name,
+                                                  system.metabolites[i].id, _private.metaboliteRadius));
       }
   }
   function scale_radius(system){
@@ -59,18 +60,19 @@ var System = function(attributes, system){
     var flux_array = [];
     for (var i = 0; i< system.reactions.length; i ++){
         flux_array.push(system.reactions[i].flux_value);
-    };
+    }
     var largest = Math.max.apply(Math, flux_array);
     var r = d3.scale.linear().domain([0,largest]).range([0,15]);
-    return r
+    return r;
   }
   function buildReactions(system){
       var tempLinks = [];
       // scale_radius
-      radiusScale = scale_radius(system);
+      var radiusScale = scale_radius(system);
       // loop and bind reaction data to reaction node
+      var s, t;
       for (var i = 0; i<system.reactions.length; i++){
-            private.nodesSet.push(new Reaction(system.reactions[i].name,
+            _private.nodesSet.push(new Reaction(system.reactions[i].name,
                                                 system.reactions[i].id,
                                                 radiusScale,
                                                 system.reactions[i].flux_value
@@ -78,27 +80,26 @@ var System = function(attributes, system){
 
           // assign metabolite source and target for each reaction
           var m = Object.keys(system.reactions[i].metabolites);
+
           for (var k = 0; k<m.length; k++){
               if(system.reactions[i].metabolites[m[k]]>0){
-                var s = system.reactions[i].id;
-                var t = m[k];
+                s = system.reactions[i].id;
+                t = m[k];
               }else{
-                var s = m[k];
-                var t = system.reactions[i].id;
+                s = m[k];
+                t = system.reactions[i].id;
               }
 
               tempLinks.push({id: s+"-"+t, source: s, target: t});
           }
       }
-
-      var nodesMap = map(private.nodesSet);
-      //console.log(nodesMap);
+      var nodesMap = map(_private.nodesSet);
       for (var j=0; j<tempLinks.length;j++){
           //ineffiecient, but will do for now
-          var s = private.nodesSet[nodesMap[tempLinks[j].source]];
+          s = _private.nodesSet[nodesMap[tempLinks[j].source]];
 
-          var t =  private.nodesSet[nodesMap[tempLinks[j].target]];
-          private.linkSet.push({id: s.getID()+"-"+t.getID(), source: s, target: t});
+          t =  _private.nodesSet[nodesMap[tempLinks[j].target]];
+          _private.linkSet.push({id: s.getID()+"-"+t.getID(), source: s, target: t});
       }
 
   }
@@ -107,13 +108,13 @@ var System = function(attributes, system){
       var markers = [
                       {id: "triangle", path: 'M 0,0 m -5,-5 L 5,0 L -5,5 Z', viewbox: '-5 -5 10 10' }
                     ];
-      var marker = private.system.insert("g")
+      var marker = _private.system.insert("g")
                             .attr("class", "markers")
                             .selectAll(".marker")
                             .data(markers)
                             .enter()
                               .append('svg:marker')
-                              .attr('id', function(d){ return d.id})
+                              .attr('id', function(d){ return d.id;})
                               .attr('markerHeight', 5)
                               .attr('markerWidth', 5)
                               .attr('markerUnits', 'strokeWidth')
@@ -122,20 +123,20 @@ var System = function(attributes, system){
                               .attr('refY', 0)
                               .attr('fill', palette.linktest)
                               .attr('opacity', 1)
-                              .attr('viewBox', function(d){ return d.viewbox })
+                              .attr('viewBox', function(d){ return d.viewbox; })
                               .append('svg:path')
-                              .attr('d', function(d){ return d.path });
+                              .attr('d', function(d){ return d.path; });
   }
   //draw function
   function draw(){
     // draw links
     addMarkers();
-    private.links = private.links
+    _private.links = _private.links
       .enter()
       .append("g")
       .attr("class", "link")
       .attr("id", function(d) {
-          return "id-" + d.id
+          return "id-" + d.id;
       })
       .insert("line")
       .style("stroke", "#ccc")
@@ -143,12 +144,12 @@ var System = function(attributes, system){
       .style("opacity", 1)
       .attr("marker-end", function(d) {
           if (d.source.getType() == "r") {
-              return "url(#triangle)"
+              return "url(#triangle)";
           }
-      })
+      });
       // call draw function in reaction and metabolite node class
-      for(var i = 0; i< private.nodesSet.length; i++){
-          private.nodesSet[i].draw();
+      for(var i = 0; i< _private.nodesSet.length; i++){
+          _private.nodesSet[i].draw();
       }
 
 
@@ -163,12 +164,12 @@ var System = function(attributes, system){
       return ret;
   }
   function tick(){
-    private.nodes.attr("transform", function(d) {
+    _private.nodes.attr("transform", function(d) {
       return "translate(" + d.x + "," + d.y + ")"; });
-    private.links.attr("x1", function(d) { return d.source.x; })
+    _private.links.attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
   }
   //returns nothing as of now, everything done when you create the object
-}
+};
