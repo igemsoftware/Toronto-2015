da13c44815ffda2492a1009fd9d5c27c747f1e81
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-08-29 20:05:15 -0400

    finds common metabolite ids

--- routes/community/create/index.js
+++ routes/community/create/index.js
@@ -2,10 +2,41 @@ var router = require('express').Router();
 
 var MetabolicModel = App.Model('metabolicmodel');
 
-function createCommunity(req, res, next) {
-	res.send('Creating a community\n');
+function compareMetabolites(specieId, masterMetabolites) {
+	MetabolicModel.findOne({id: specieId}, function(err, model) {
+		model.metabolites.forEach(function(metabolite) {
+
+			if (metabolite.id in masterMetabolites) {
+				console.log(specieId, metabolite.id);
+			}
+		})
+	});
 }
 
-router.post('/', createCommunity)
+function createCommunity(req, res, next) {
+	MetabolicModel.findOne({id: req.body.master}, function(err, model) {
+		if (err) {
+			res.status(500).send('500 Internal Server Error');
+			return;
+		}
+
+		var masterMetabolites = new Object();
+
+		model.metabolites.forEach(function(metabolite) {
+			masterMetabolites[metabolite.id] = metabolite;
+		});
+
+		req.body.species.forEach(function(specie) {
+			if (specie !== req.body.master) {
+				console.log(specie);
+				compareMetabolites(specie, masterMetabolites);
+			}
+		})
+
+		res.send(masterMetabolites);
+	});
+}
+
+router.post('/', createCommunity);
 
 module.exports = router;
