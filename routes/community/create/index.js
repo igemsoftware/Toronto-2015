f1173fea7800089b1d182f597a6508e3ec6bd01b
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-08-30 12:20:20 -0400

    some refactoring

--- routes/community/create/index.js
+++ routes/community/create/index.js
@@ -3,10 +3,16 @@ var router = require('express').Router();
 var MetabolicModel = App.Model('metabolicmodel');
 
 function compareMetabolites(specieId, masterMetabolites) {
+
+	// Build dict with id:metabolite
+	var masterMetabolitesIds = new Object();
+	masterMetabolites.forEach(function(metabolite) {
+		masterMetabolitesIds[metabolite.id] = metabolite;
+	});
+
 	MetabolicModel.findOne({id: specieId}, function(err, model) {
 		model.metabolites.forEach(function(metabolite) {
-
-			if (metabolite.id in masterMetabolites) {
+			if (metabolite.id in masterMetabolitesIds) {
 				console.log(specieId, metabolite.id);
 			}
 		})
@@ -14,26 +20,28 @@ function compareMetabolites(specieId, masterMetabolites) {
 }
 
 function createCommunity(req, res, next) {
-	MetabolicModel.findOne({id: req.body.master}, function(err, model) {
+	// Retrieve Master model for Master Metabolites
+	MetabolicModel.findOne({id: req.body.master}, function(err, masterModel) {
 		if (err) {
 			res.status(500).send('500 Internal Server Error');
 			return;
 		}
 
-		var masterMetabolites = new Object();
-
-		model.metabolites.forEach(function(metabolite) {
-			masterMetabolites[metabolite.id] = metabolite;
-		});
+		// var masterMetabolites = new Object();
+		//
+		// model.metabolites.forEach(function(metabolite) {
+		// 	masterMetabolites[metabolite.id] = metabolite;
+		// });
 
 		req.body.species.forEach(function(specie) {
+			// Compare everything but master with master
 			if (specie !== req.body.master) {
 				console.log(specie);
-				compareMetabolites(specie, masterMetabolites);
+				compareMetabolites(specie, masterModel.metabolites);
 			}
 		})
 
-		res.send(masterMetabolites);
+		res.send('Creating community');
 	});
 }
 
