6661b86fc041f2647968604f0fd079037e8afe18
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-09-17 22:05:29 -0400

    fancier add specie modal with accordion

--- web-app/src/app/app.js
+++ web-app/src/app/app.js
@@ -1,24 +1,24 @@
 'use strict';
 
-angular.module('ConsortiaFlux', ['ui.router', 'ngTable', '720kb.tooltips', 'angularModalService'])
+angular.module('ConsortiaFlux', ['ui.router', 'ngTable', '720kb.tooltips', 'angularModalService', 'ui.bootstrap'])
 
 .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-    .state('main', {
-        templateUrl: 'app/templates/main.html'
-    })
-    .state('main.home', {
-        url: '/',
-        templateUrl: 'app/pages/home/home.html',
-        controller: 'HomeCtrl'
-    })
-    .state('main.flux', {
-        url: '/flux',
-        templateUrl: 'app/pages/flux/flux.html',
-        // controller: 'FluxCtrl'
-    });
+        .state('main', {
+            templateUrl: 'app/templates/main.html'
+        })
+        .state('main.home', {
+            url: '/',
+            templateUrl: 'app/pages/home/home.html',
+            controller: 'HomeCtrl'
+        })
+        .state('main.flux', {
+            url: '/flux',
+            templateUrl: 'app/pages/flux/flux.html',
+            // controller: 'FluxCtrl'
+        });
 })
 
 .controller('BodyCtrl', ['$scope', function($scope) {
--- web-app/src/app/modals/addspecie/addspecie.controller.js
+++ web-app/src/app/modals/addspecie/addspecie.controller.js
@@ -6,19 +6,28 @@ angular.module('ConsortiaFlux')
 .controller('AddSpecieModal', ['$scope', '$http', 'UrlProvider', 'close',
     function($scope, $http, UrlProvider, close) {
 
-    var result = 'wheee';
-    $scope.loading = true;
+        var result = 'wheee';
+        $scope.loading = true;
 
-    $http.get(UrlProvider.baseUrl + '/model/retrieve').then(function(data) {
-        $scope.loading = false;
-        $scope.species = data.data;
-    });
+        $http.get(UrlProvider.baseUrl + '/model/retrieve').then(function(data) {
+            $scope.loading = false;
+            $scope.species = data.data;
 
-    $scope.display = true;
+            $scope.groups = [{
+                title: 'Dynamic Group Header - 1',
+                content: 'Dynamic Group Body - 1'
+            }, {
+                title: 'Dynamic Group Header - 2',
+                content: 'Dynamic Group Body - 2'
+            }];
+        });
 
-    $scope.close = function(result) {
-        $scope.display = false;
-        close(result, 200); // close, but give 200ms for bootstrap to animate
-    };
+        $scope.display = true;
 
-}]);
+        $scope.close = function(result) {
+            $scope.display = false;
+            close(result, 200); // close, but give 200ms for bootstrap to animate
+        };
+
+    }
+]);
